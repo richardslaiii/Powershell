b3c5d8b8ff807d775f623bc75cc1f3e02fb79182
richardslaiii <richardslaiii@gmail.com>
2020-04-23 14:25:38 -0400

    Create get-onlinecomputers.ps1

---
+++ get-onlinecomputers.ps1
@@ -0,0 +1,20 @@
+$Computers = "localhost","","test"
+ 
+$Onlinecomputers = New-Object System.Collections.ArrayList
+ 
+$Offlinecomputers = New-Object System.Collections.ArrayList
+ foreach ($computer in $computers)
+ {
+ If (Test-Connection $Computer -Count 1 -Delay 1 -Quiet -ErrorAction silentlycontinue)
+ 
+ {
+ $Onlinecomputers.add($Computer) | out-null
+ }
+ 
+ else {
+ $Offlinecomputers.add($Computer) | Out-Null
+ }
+ }
+ $onlinecomputers | Add-Member -MemberType Noteproperty -Name 'computer' -Value "$computer"
+ 
+ $onlinecomputers
